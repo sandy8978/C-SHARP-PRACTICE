7e983a96ceaeba042fe5c0e919143d8a2e42be46
sandeep <142823664+sandy8978@users.noreply.github.com>
2024-02-08 12:46:06 +0530

    Add files via upload

---
+++ sample4.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplications
+{
+    class sample4
+    {
+      public static void Main()
+        {
+            Console.WriteLine("enter Product Name");
+            string PName = Console.ReadLine();
+            Console.WriteLine("Enter Price");
+            double price = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter Quality");
+            int Qty = Convert.ToInt32(Console.ReadLine());
+            int totalValue = (int)(price * Qty);
+            string tot = Convert.ToString(totalValue);
+            Console.WriteLine($"Product Name is: {PName} and Total value is: {tot}");
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ sample5.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplications
+{
+    class sample5
+    {
+       public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter UserName");
+            string UName = Console.ReadLine();
+            Console.WriteLine("Enter Password");
+            string password = Console.ReadLine();
+
+            if (UName == "Nag@gmail.com" && password == "Nag@123")
+            {
+                Console.WriteLine("Welcome To My website:"   +UName);
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("Sorry !  No User Exist");
+                Console.WriteLine("Please Try Again ");
+                Console.ReadKey();
+
+            }
+           
+        }
+    }
+}
